5f19b6e5c9c21d2900f04e7c0e2ccdc469e4dbaf
carlos-ekuore <carlos.delafuente@ekuore.com>
2022-04-10 19:19:54 +0200

    Added blink on pulse

--- bike/bike.ino
+++ bike/bike.ino
@@ -9,15 +9,19 @@ BLEByteCharacteristic resistanceCharacteristic("19B10003-E8F2-537E-4F6C-D104768A
 
 
 int odometer = 15;    // Analog pin 1 digital pin 15 first jack
+int led = LED_BUILTIN;
 const int AVG = 4;    // Number of reads necessary to count as a pulse
 int counter = 0; unsigned long startTime; unsigned long runTime; unsigned long duration;
-
+int led_delay = 0;
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   // make the pushbutton's pin an input:
   pinMode(odometer, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
+  
   // begin initialization
   if (!BLE.begin()) {
     Serial.println("starting BLE failed!");
@@ -52,16 +56,20 @@ void loop() {
     Serial.println(central.address());
     // while the central is still connected to peripheral:
     while (central.connected()) {
+      if(led_delay == 1000){digitalWrite(led, LOW);}
+      else{led_delay++;}
       // read the input pin:
       int odometer_state = digitalRead(odometer);
       if (!digitalRead(odometer)){counter++;}   // if pulse read increase counter
       else{counter = 0;}
       if(counter==AVG){
+        digitalWrite(led, HIGH);
+        led_delay = 0;
         runTime = millis();
         duration = runTime - startTime;
         startTime = millis();
-        Serial.println(duration);
         if(duration < 2550){    
+          Serial.println(duration);
           speedCharacteristic.writeValue(duration/10); // Send cents of a second
         }
       }
